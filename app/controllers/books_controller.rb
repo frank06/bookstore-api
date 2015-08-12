a9fd4c2dcefe58b461ba5bcd65c9017880ff16fd
frank06 <me@frank06.net>
2015-08-12 00:47:46 -0300

    limit books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,8 @@ class BooksController < ApplicationController
 
   # GET /books
   def index
-    @books = Book.all
-
-    render json: @books
+    @books = Book.limit(params[:limit])
+    render json: @books, meta: { total: Book.count }
   end
 
   # GET /books/1
